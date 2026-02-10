c1f4375aac0c305bd785e988e8e89d005d8d6774
steigerbalett <40899113+steigerbalett@users.noreply.github.com>
2026-02-10 14:24:16 +0100

    Update: Verwende Certificate Bundle statt hartkodiertem Root-Zertifikat für Update-Check

--- update_check.h
+++ update_check.h
@@ -11,32 +11,6 @@
 #define GITHUB_REPO_NAME "lacrosse2mqtt"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_REPO_OWNER "/" GITHUB_REPO_NAME "/releases/latest"
 
-// GitHub Root CA Certificate (DigiCert Global Root CA)
-// Gültig bis: 2031
-const char* github_root_ca = \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
-"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
-"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
-"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
-"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
-"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
-"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
-"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
-"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
-"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
-"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
-"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
-"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
-"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
-"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
-"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
-"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
-"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
-"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
-"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
-"-----END CERTIFICATE-----\n";
-
 struct UpdateInfo {
     bool available;
     String latestVersion;
@@ -67,18 +41,15 @@ bool checkForUpdate() {
     WiFiClientSecure *client = new WiFiClientSecure;
     
     if (client) {
-        // Setze GitHub Root CA Zertifikat
-        client->setCACert(github_root_ca);
-        
-        // Verwende das Bundle (größer):
-        // client->setCACertBundle(ca_cert_bundle_start);
+        // Verwende das Certificate Bundle für bessere Kompatibilität
+        client->setCACertBundle(ca_cert_bundle_start);
         
         HTTPClient http;
         
         Serial.println("Checking for updates...");
         Serial.println("URL: " + String(GITHUB_API_URL));
         
-        // Verwende HTTPS mit Zertifikat
+        // Verwende HTTPS mit Certificate Bundle
         if (http.begin(*client, GITHUB_API_URL)) {
             http.addHeader("Accept", "application/vnd.github+json");
             http.addHeader("User-Agent", "LaCrosse2MQTT-Updater");
@@ -181,7 +152,8 @@ bool installUpdate() {
     WiFiClientSecure *client = new WiFiClientSecure;
     
     if (client) {
-        client->setCACert(github_root_ca);
+        // Verwende das Certificate Bundle
+        client->setCACertBundle(ca_cert_bundle_start);
         
         HTTPClient http;
         
